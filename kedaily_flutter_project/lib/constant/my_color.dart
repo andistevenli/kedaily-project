c2a508c7c707a0aa541b5d0a419cdf466229b78e
Andi Stevenli <ndstevenli@gmail.com>
2023-06-10 10:26:12 +0700

    create my_color.dart

---
+++ kedaily_flutter_project/lib/constant/my_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static const primaryColor = Colors.blue;
+  static const editColor = Colors.yellow;
+  static const deleteColor = Colors.red;
+  static const successColor = Colors.green;
+  static const whiteColor = Colors.white;
+  static const infoColor = Colors.black;
+  static const subInfoColor = Colors.grey;
+}
