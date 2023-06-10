a7db6a5651cac4ee1bc3a42dc1e3542a25d3e260
Andi Stevenli <ndstevenli@gmail.com>
2023-06-10 12:57:14 +0700

    setup internet user permission, minSdkVersion, multi dex, dan mengkoneksikan dengan supabase

--- kedaily_flutter_project/lib/main.dart
+++ kedaily_flutter_project/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'kedaily.dart';
+import 'model/service/supabase_service.dart';
 
 void main() async {
+  await SupabaseService.initSupabase();
   runApp(const Kedaily());
 }
